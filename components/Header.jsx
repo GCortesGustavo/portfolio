aacc478a6756099e0164366a2bd77cfde768b996
GCortesGustavo <gustavocortes934@gmail.com>
2023-12-17 14:01:06 -0300

    Header con Navbar terminada, se agrega scroll progress y underline sobre paths

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,55 @@
+"use client";
+import React, {useState, useEffect} from "react";
+
+//components
 import ThemeToggler from "./ThemeToggler"
+import Logo from "./ui/Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+import { usePathname } from "next/navigation";
 
 
 const Header = () => {
+    const [header, setHeader] = useState(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            window.scrollY > 50 ? setHeader(true) : setHeader(false);
+        };
+        
+        //add event
+        window.addEventListener("scroll", handleScroll)
+        //remove
+        return () => window.removeEventListener("scroll", handleScroll);
+    })
     return (
-        <header>
-            <ThemeToggler />
+        <header 
+        className={`${
+            header 
+            ? "py-4 bg-white shadow-lg dark:bg-accent sticky top-0 z-30 transition-all" 
+            : "py-6 dark:bg-transparent"
+            } sticky top-0 z-30 transition-all ${pathname === "/" && "bg-[#fef9ff]"}`}>
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    <Logo />
+                    <div className="flex items-center gap-x-6">
+                        {/*Nav*/}
+                        <Nav 
+                            containerStyles="hidden xl:flex gap-x-8 items-center" 
+                            linkStyles="relative hover:text-primary transition-all" 
+                            underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"/>
+                        <ThemeToggler />
+                        {/*Mobile Nav*/}
+                        <div className="xl:hidden">
+                            <MobileNav />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
+
 
 export default Header
