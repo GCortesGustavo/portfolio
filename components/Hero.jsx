d5c4c6659643c20e692cddae5e0c6ceac457e1aa
Gustavo Cortes <gustavocortes934@gmail.com>
2023-12-19 17:29:37 -0300

    Se termina el Hero. Falta afinar los pixeles de la imagen y arreglar detalles de numeros

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -35,7 +35,18 @@ const Hero = () => {
                     <Socials containerStyles="flex gap-x-6 mx-auto xl:mx:0" iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"/>
                 </div>
                 {/*Image*/}
-                <div className="hidden xl:flex relative">Image</div>
+                <div className="hidden xl:flex relative">
+                    {/*Badge 1*/}
+                    <Badge containerStyles="absolute top-[24%] -left-[5rem]" icon={<RiBriefcase4Fill />} endCountNum={1} badgeText="Years Of Experience"/>
+                    {/*Badge 2*/}
+                    <Badge containerStyles="absolute top-[80%] -left-[1rem]" icon={<RiTodoFill />} endCountNum={6} endCountText="k" 
+                    badgeText="Finished Proyects"/>
+                    {/*Badge 3*/}
+                    <Badge containerStyles="absolute top-[55%] -right-8" icon={<RiTeamFill />} endCountNum={9} endCountText="k"
+                    badgeText="Happy Clients"/>
+                    <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+                    <DevImg containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom" imgSrc="/hero/developer1.png"/>
+                </div>
             </div>
             {/*Icon*/}
             <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
