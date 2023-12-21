b2e5c0ceae177c0728856aa2942123bd1e7a6af6
GCortesGustavo <gustavocortes934@gmail.com>
2023-12-21 00:49:53 -0300

    Inicio de servicios

---
+++ components/Services.jsx
@@ -0,0 +1,21 @@
+import { GanttChartSquare, Blocks, Gem} from "lucide-react";
+import { Card, 
+    CardContent, 
+    CardDescription, 
+    CardHeader,
+    CardTitle 
+} from "./ui/card";
+
+
+const servicesData = [
+    {
+        icon: <GanttChartSquare size={15}/>
+    }
+]
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
