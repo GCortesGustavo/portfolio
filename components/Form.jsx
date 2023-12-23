97c7e862aecaa795a5938a6872d15a1c0563310f
GCortesGustavo <gustavocortes934@gmail.com>
2023-12-23 20:31:27 -0300

    Formulario funcionando

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,34 +1,51 @@
 "use client"
 
 import { Button } from "./ui/button";
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { User, MailIcon, ArrowRightIcon, MessageSquare} from "lucide-react"
+import { MailIcon, ArrowRightIcon, MessageSquare} from "lucide-react"
+
 
 const Form = () => {
-  return (
-    <form className="flex flex-col gap-y-4">
-        {/*Input */}
-        <div className="relative flex items-center">
-            <Input type="text" id="name" placeholder="Name" />
-            <User className="absolute right-6" size={20} />
-        </div>
-        {/*Input */}
-        <div className="relative flex items-center">
-            <Input type="email" id="email" placeholder="Email" />
-            <MailIcon className="absolute right-6" size={20} />
-        </div>
-        {/*Input */}
-        <div className="relative flex items-center">
-            <Textarea placeholder="Type Your Message Here." />
-            <MessageSquare className="absolute top-4 right-6" size={20} />
-        </div>
-        <Button className="flex items-center gap-x-1 max-w-[166px]">
-            Let's Talk 
+    const [state, handleSubmit] = useForm("mnqeawal");
+    if (state.succeeded) {
+        return <p>Thank you for getting in touch with me.!</p>;
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
+            <div className="relative flex items-center">
+                        <Input type="email" id="email" placeholder="Email" name="email" />
+                        <MailIcon className="absolute right-6" size={20} />
+            </div>
+
+            <ValidationError 
+                prefix="Email" 
+                field="email"
+                errors={state.errors}
+            />
+            
+            <div className="relative flex items-center">
+                <Textarea placeholder="Type Your Message Here." id="message" name="message"/>
+                <MessageSquare className="absolute top-4 right-6" size={20} />
+            </div>
+
+            <ValidationError 
+                prefix="Message" 
+                field="message"
+                errors={state.errors}
+            />
+
+            <Button type="submit" disabled={state.submitting} className="flex items-center gap-x-1 max-w-[166px]">
+            Let's Talk
             <ArrowRightIcon size={20}/>
-        </Button>
-    </form>
-  )
+            </Button>
+        </form>
+    );
 }
+
 
 export default Form
